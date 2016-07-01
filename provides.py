a5ed48a3adece79f40224b8c72f64e5679c3eda7
Alex Kavanagh <alex@ajkavanagh.co.uk>
2016-07-01 14:49:41 +0000

    Add LICENSE, copyright headers and docstrings
    This patch adds the Apache2.0 LICENSE file and appropriate copyright headers to
    the two .py files.
    
    Also, docstrings are added to the classes and appropriate functions.

--- provides.py
+++ provides.py
@@ -1,28 +1,74 @@
+# Copyright 2016 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
 import charms.reactive as reactive
 import charmhelpers.core.hookenv as hookenv
 
 
 class BarbicanProvides(reactive.RelationBase):
+    """This is the barbican-{type}hsm-plugin end of the relation
+
+    The HSM provider needs to set it's name (which may be relevant) and
+    also provide any plugin data to Barbican.
+
+    The plugin data is:
+
+    {
+         "library_path": <library path of PKCS#11 libary>,
+         "login": the pin/password for accessing the PKCS#11 token,
+         "slot_id": <int: slot_id of the token for barbican to use>
+    }
+    """
     scope = reactive.scopes.GLOBAL
 
     # These remote data fields will be automatically mapped to accessors
     # with a basic documentation string provided.
-    auto_accessors = ['test']
+    auto_accessors = []
 
     @reactive.hook('{provides:barbican-hsm-plugin}-relation-joined')
     def joined(self):
         self.set_state('{relation_name}.connected')
-        hookenv.log("BarbicanProvides.joined() ran")
+        self.set_state('{relation_name}.available')
 
     @reactive.hook('{provides:barbican-hsm-plugin}-relation-changed')
     def changed(self):
-        hookenv.log("BarbicanProvides.changed() ran")
+        pass
 
     @reactive.hook('{provides:barbican-hsm-plugin}-relation-{broken,departed}')
     def departed(self):
-        hookenv.log("BarbicanProvides.departed() ran")
-        self.remove_state('{relation_name}.connected')
         self.remove_state('{relation_name}.available')
+        self.remove_state('{relation_name}.connected')
 
     def set_name(self, name):
-        self.set_remote(name=name)
+        """Set the name of the plugin.
+
+        :param name: string
+        """
+        self.set_remote(_name=name)
+
+    def set_plugin_data(self, data):
+        """Set the plugin data for the remote (Barbican charm) to be able to
+        configure barbican to use the HSM that is installed alongside it.
+
+        NOTE that the data is wrapped in a dictionary, converted to JSON and
+        then placed in the juju remote variable.  The other 'end' unpacks this
+        and provides the original data to Barbican charm.
+
+        Thus data has to be JSONable.
+
+        :param data: object that describes the plugin data to be sent.
+        """
+        self.set_remote(_plugin_data=json.dumps({"data": data}))
--- requires.py
+++ requires.py
@@ -1,31 +1,77 @@
+# Copyright 2016 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
 import charms.reactive as reactive
 import charmhelpers.core.hookenv as hookenv
 
 
 class BarbicanRequires(reactive.RelationBase):
+    """The is the Barbican 'end' of the relation.
+
+    The auto accessors are _name and _plugin_data.  They are underscored as for
+    some reason RelationBase only provides these as 'calls'; i.e. they have to
+    be used as `self._name()`.  This class therefore provides @properties
+    `name` and `plugin_data` that can be used directly.
+
+    The `plugin_data` property also deserialises the _plugin_data from the
+    plugin.
+    """
     scope = reactive.scopes.GLOBAL
 
     # These remote data fields will be automatically mapped to accessors
     # with a basic documentation string provided.
-    auto_accessors = ['name']
+    auto_accessors = ['_name', '_plugin_data']
 
     @reactive.hook('{requires:barbican-hsm-plugin}-relation-joined')
     def joined(self):
         self.set_state('{relation_name}.connected')
-        hookenv.log("BarbicanRequires.joined() ran")
         self.update_status()
 
     @reactive.hook('{requires:barbican-hsm-plugin}-relation-changed')
     def changed(self):
-        hookenv.log("BarbicanRequires.changed() ran")
         self.update_status()
 
     @reactive.hook('{requires:barbican-hsm-plugin}-relation-{broken,departed}')
     def departed(self):
-        hookenv.log("BarbicanRequires.departed() ran")
         self.remove_state('{relation_name}.connected')
         self.remove_state('{relation_name}.available')
 
     def update_status(self):
-        if self.name is not None:
+        if self._name() is not None and self._plugin_data() is not None:
             self.set_state('{relation_name}.available')
+
+    @property
+    def name(self):
+        """Returns a string of the name or None if it doesn't exist"""
+        return self._name()
+
+    @property
+    def plugin_data(self):
+        """Return the plugin_data from the plugin if it is available.
+
+        The format of the data returned is:
+        {
+             "library_path": <library path of PKCS#11 libary>,
+             "login": the pin/password for accessing the PKCS#11 token,
+             "slot_id": <int: slot_id of the token for barbican to use>
+        }
+
+        :returns: data object that was passed.
+        """
+        data = self._plugin_data()
+        if data is None:
+            return
+        return json.loads(data)["data"]
